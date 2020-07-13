fb6df82387a61ef9ab666a2ab4b07f6f0eed80e7
Albertus Andito <aandito7@gmail.com>
2020-07-13 08:51:05 +0100

    minor

--- src/main/java/gg/GG.java
+++ src/main/java/gg/GG.java
@@ -1,6 +1,7 @@
 package gg;
 
 import robocode.*;
+import robocode.util.Utils;
 
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -22,18 +23,25 @@ public class GG extends AdvancedRobot {
 
         //
         boolean isAlive = true;
+
+        turnRadarRight(720);
+
         while(isAlive) {
             vm.updateStatus();
 
             //Choose nearest enemy to fire (scanning)
-            vm.enemyToShoot();
+            EnemyInfo enemy = vm.enemyToShoot();
 
-            //fire and rotate gun based on enemyToShoot
+            //rotate and fire gun based on enemyToShoot
+            turnGunRight(enemy.angle);
+            fire(1); //maybe we can set the bullet power based on enemy's distance
 
             //Choose direction to move
-            vm.nextMovement();
+            Movement movement = vm.nextMovement();
 
             //move based on nextMovement
+
+            //Possibly need to scan environment again here
         }
     }
 
@@ -41,7 +49,13 @@ public class GG extends AdvancedRobot {
         vm.store(e);
     }
 
+    public void onRobotDeath(RobotDeathEvent e) {
+        vm.removeEnemy(e.getName());
+    }
 
+    enum radarBehaviors {
+        MELEE, LOCK
+    }
 
     //Virtual Map
     class VM {
@@ -49,12 +63,29 @@ public class GG extends AdvancedRobot {
         //Sorted on enemy's next position
         Map<String, List<ScannedRobotEvent>> map = new HashMap<>();
 
+        radarBehaviors radarBehavior = radarBehaviors.MELEE;
+
         void store(ScannedRobotEvent e) {
             List events = map.getOrDefault(e.getName(), new LinkedList<>());
             events.add(e);
             //Only keep 10 last events
-
+            if (events.size() > 10) {
+                events.remove(0);
+            }
             map.put(e.getName(), events);
+            if (map.size() == 1) {
+                radarBehavior = radarBehaviors.LOCK;
+            }
+
+            //lock radar on enemy
+            if (radarBehavior == radarBehaviors.LOCK) {
+                double radarTurn =
+                        // Absolute bearing to target
+                        getHeadingRadians() + e.getBearingRadians()
+                        // Subtract current radar heading to get turn required
+                        - getRadarHeadingRadians();
+                setTurnRadarRightRadians(Utils.normalRelativeAngle(radarTurn));
+            }
 
         }
 
@@ -70,6 +101,13 @@ public class GG extends AdvancedRobot {
 
         void updateStatus() {
             //our own tank status
+        }
+
+        void removeEnemy(String name) {
+            map.remove(name);
+            if (map.size() == 1) {
+                radarBehavior = radarBehaviors.LOCK;
+            }
         }
     }
 
